bb30c0b530655333b5eced6b7ca0b16a85c850b3
rjmlopes <rjmlopes@cercredrj008.alerj.gov.br>
2017-10-17 19:01:26 -0200

    tela calculo

--- TabalhoEstatistica/src/control/MainApp.java
+++ TabalhoEstatistica/src/control/MainApp.java
@@ -5,11 +5,15 @@ import java.util.Date;
 import model.Transacao;
 import model.Utility;
 import model.dao.TransacaoDao;
+import view.CalculoView;
 import view.MainFrame;
+import view.TransacaoDialog;
 
 public class MainApp {
 
 	public static void main(String[] args) {
+		//new TransacaoDialog(null);
+		//new CalculoView(null);
 		new MainFrame();
 	}
 }
